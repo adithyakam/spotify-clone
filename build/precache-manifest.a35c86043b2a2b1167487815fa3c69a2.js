d25dc7989e7996bce26c30303d9f061511a0599f
adithyakam <50355901+adithyakam@users.noreply.github.com>
2020-09-26 20:32:58 +0530

    adding build
    adding build

---
+++ build/precache-manifest.a35c86043b2a2b1167487815fa3c69a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f779c3a8705710e8b35993f9e308db6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "678f304a59d3e760ef9f",
+    "url": "/static/css/main.edd20c43.chunk.css"
+  },
+  {
+    "revision": "f57987da6c2d8717af3d",
+    "url": "/static/js/2.c72d82cb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.c72d82cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "678f304a59d3e760ef9f",
+    "url": "/static/js/main.a38c9516.chunk.js"
+  },
+  {
+    "revision": "6ca49defa095afd3e9c1",
+    "url": "/static/js/runtime-main.f9911b94.js"
+  }
+]);
